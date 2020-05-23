871affd61ab81e77919a88f020fa5cac2a9fa9d1
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-05-23 10:50:00 +0200

    p5js project 7 update

--- p5JS/p5Studydocuments/07-Array&Loop/07arrayLoop.js
+++ p5JS/p5Studydocuments/07-Array&Loop/07arrayLoop.js
@@ -1,8 +1,41 @@
-let timeoutID = setTimeout(bye, 10000);
-console.log("hello");
+let bubbles = [];
 
-clearTimeout(timeoutID);
+function setup(){
+    createCanvas(600, 600);
+    for (let i = 0; i < 20; i++){// adding loops of 5 bubble
+    let x = random(width)
+    let y = random(height)
+    let r = random(10, 50)
+    let b = new Bubble(x, y, r);
+    bubbles.push(b);// pushing b bubble in side array
+    }
+}
+function draw(){
+    background(0);
+    // loop of drawing the bubbles.
+    for (let i = 0; i < bubbles.length; i++){
+        bubbles[i].object1();
+        bubbles[i].move();
+    }
+}
 
-function bye(){
-  console.log("goodBye");
+class Bubble {
+    
+    constructor(tempX, tempY, tempS){
+        this.x = tempX;
+        this.y = tempY;
+        this.s = tempS; 
+    }
+    
+    object1(){
+        stroke(255);
+        strokeWeight(3);
+        fill(random(255),0, random(200), 80);
+        ellipse(this.x, this.y, this.s * 2);
+    }
+    
+    move(){
+        this.x = this.x + random (-5, 5);
+        this.y = this.y + random (-5, 5);
+}
 }
